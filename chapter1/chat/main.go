ab6da53b6d4d5a070f72c1803cef74055032c2bb
koshigoe <koshigoeb@gmail.com>
2016-01-23 14:44:09 +0900

    Add template.

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-                  <html>
-                    <head>
-                      <title>チャット</title>
-                    </head>
-                    <body>
-                      チャットしましょう！
-                    </body>
-                  </html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP は HTTP リクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil) // 監訳注1: t.templ.Execute の戻り値はチェックすべきです。
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Web サーバを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
