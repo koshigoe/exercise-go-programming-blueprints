46ef728e1f7b91c95d11e2d6f8b2f616d50c4c8a
koshigoe <koshigoeb@gmail.com>
2016-01-23 18:49:39 +0900

    Handle chat room.

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -23,7 +23,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// チャットルームを開始します
+	go r.run()
 
 	// Web サーバを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
