5f5e71c76c8edcdad0f748a74852daa7413df18b
koshigoe <koshigoeb@gmail.com>
2016-01-23 20:10:15 +0900

    Use trace package.

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/websocket"
+	"github.com/koshigoe/exercise-go-programming-blueprints/chapter1/trace"
 )
 
 type room struct {
@@ -15,6 +16,8 @@ type room struct {
 	leave chan *client
 	// clients には在室しているすべてのクライアントが保持されます。
 	clients map[*client]bool
+	// tracer はチャットルーム上で行われた操作のログを受け取ります。
+	tracer trace.Tracer
 }
 
 func (r *room) run() {
@@ -23,20 +26,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました: ", string(msg))
 			// すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
