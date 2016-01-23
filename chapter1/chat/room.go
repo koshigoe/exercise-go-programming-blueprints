c26ad0359fa279ec82287913e6c2774b76adc33e
koshigoe <koshigoeb@gmail.com>
2016-01-23 17:55:31 +0900

    Add member to room.

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// forward は他のクライアントに転送するたえのメッセージを保持するチャネルです。
 	forward chan []byte
+	// join はチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	// leave はチャットルームから退室しようとしているクライアントのためのチャネルです。
+	leave chan *client
+	// clients には在室しているすべてのクライアントが保持されます。
+	clients map[*client]bool
 }
