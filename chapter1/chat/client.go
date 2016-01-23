b74a4fe36322a8c4f61a32a20a415c9b1c3461b3
koshigoe <koshigoeb@gmail.com>
2016-01-23 17:50:46 +0900

    Add read/write method to client.

--- chapter1/chat/client.go
+++ chapter1/chat/client.go
@@ -13,3 +13,23 @@ type client struct {
 	// room はこのクライアントが参加しているチャットルームです。
 	room *room
 }
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
