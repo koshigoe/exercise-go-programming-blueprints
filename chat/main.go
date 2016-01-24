d41d841266f50abd93bb58330d211bb33cb96ba7
koshigoe <koshigoeb@gmail.com>
2016-01-24 19:08:00 +0900

    Use gravatar

--- chat/main.go
+++ chat/main.go
@@ -46,7 +46,7 @@ func main() {
 		github.New(os.Getenv("GITHUB_APP_ID"), os.Getenv("GITHUB_APP_SECRET"), "http://localhost:8080/auth/callback/github"),
 		google.New(os.Getenv("GOOGLE_APP_ID"), os.Getenv("GOOGLE_APP_SECRET"), "http://localhost:8080/auth/callback/google"),
 	)
-	r := newRoom(UseAuthAvatar)
+	r := newRoom(UseGravatar)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
