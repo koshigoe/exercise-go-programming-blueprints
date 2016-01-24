f3a41f4dbd5b48c8f2d7809185476c7edac31fc7
koshigoe <koshigoeb@gmail.com>
2016-01-24 19:04:54 +0900

    Add GravatarAvatar

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -23,3 +23,16 @@ func TestAuthAvatar(t *testing.T) {
 		}
 	}
 }
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvatar GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"email": "MyEmailAddress@example.com"}
+	url, err := gravatarAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("GravatarAvatar.GetAvatarURL はエラーを返すべきではありません")
+	}
+	if url != "//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346" {
+		t.Errorf("GravatarAvatar.GetAvatarURL が %s という誤った値を返しました", url)
+	}
+}
