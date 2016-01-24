977954026a5ecf3f820c30be748b4185406fd750
koshigoe <koshigoeb@gmail.com>
2016-01-24 13:31:47 +0900

    Add login page.

--- chat/main.go
+++ chat/main.go
@@ -28,6 +28,7 @@ func main() {
 	flag.Parse() // フラグを解釈します
 	r := newRoom()
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 
 	// チャットルームを開始します
