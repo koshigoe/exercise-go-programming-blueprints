97ef629137edf1d1fcd451b53f480112a6f82a8a
koshigoe <koshigoeb@gmail.com>
2016-01-24 13:52:51 +0900

    Add /auth/ endpoint.

--- chat/auth.go
+++ chat/auth.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"strings"
+	"log"
+	"fmt"
 )
 
 type authHandler struct {
@@ -24,4 +27,19 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// loginHandler はサードパーティのログインの処理を受け持ちます。
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
 }
--- chat/main.go
+++ chat/main.go
@@ -29,6 +29,7 @@ func main() {
 	r := newRoom()
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// チャットルームを開始します
