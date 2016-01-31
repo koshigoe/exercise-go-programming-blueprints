1acb33929b5d752f3ac84e1978535041c1722c6f
koshigoe <koshigoeb@gmail.com>
2016-01-31 17:41:52 +0900

    Add synonyms

---
+++ thesaurus/bighuge.go
@@ -0,0 +1,39 @@
+package thesaurus
+import (
+	"encoding/json"
+	"net/http"
+	"fmt"
+)
+
+type BigHuge struct {
+	APIKey string
+}
+
+type synonyms struct {
+	Noun *words `json:"nown"`
+	Verb *words `json:"verb"`
+}
+
+type words struct {
+	Syn []string `json:"syn"`
+}
+
+func (b *BigHuge) Synonyms(term string) ([]string, error) {
+	var syns []string
+	response, err := http.Get("http://words.bighugelabs.com/api/2/" + b.APIKey + "/" + term + "/json")
+	if err != nil {
+		return syns, fmt.Errorf("bighuge: %q の類語検索に失敗しました: %v", term, err)
+	}
+	var data synonyms
+	defer response.Body.Close()
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		return syns, err
+	}
+	if data.Noun != nil {
+		syns = append(syns, data.Noun.Syn...)
+	}
+	if data.Verb != nil {
+		syns = append(syns, data.Verb.Syn...)
+	}
+	return syns, nil
+}
